4e83aea856f7395ca990593d22f946eb9423fcd5
Marcel Suter <marcel.suter@bzz.ch>
2023-07-31 10:08:29 +0200

    initial commit

---
+++ givenname.py
@@ -0,0 +1,7 @@
+def main():
+    # write your program here and remove the line "pass"
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ givenname_test.py
@@ -0,0 +1,21 @@
+from givenname import main
+
+
+def test_step1(capsys):
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == "Urs\n"
+
+def test_step2(capsys, monkeypatch):
+    inputs = iter(['Robert', 'Daniela', 'Urs', 'Charles', 'Kyle'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == "Urs\n"
+
+def test_step3(capsys, monkeypatch):
+    inputs = iter(['Roberta', 'Urs', 'Daniela', 'Charles', 'Kyle', 2])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == "Urs\n"
