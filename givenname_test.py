f1201d562aeeafaed75ac6c5df8e1df647c9d71f
graphics80 <K.Ilkow@gmail.com>
2023-10-26 12:41:51 +0200

    Update givenname_test.py

--- givenname_test.py
+++ givenname_test.py
@@ -1,21 +1,25 @@
 from givenname import main
 
 
-def test_step1(capsys):
+def test_step1(capsys, monkeypatch):
+    inputs = iter(['Robert', 'Daniela', 'Urs', 'Charles', 'Kyle', 3])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
     assert captured.out == "Urs\n"
 
+
 def test_step2(capsys, monkeypatch):
-    inputs = iter(['Robert', 'Daniela', 'Peter', 'Charles', 'Kyle'])
+    inputs = iter(['Robert', 'Daniela', 'Urs', 'Charles', 'Kyle', 3])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
-    assert captured.out == "Peter\n"
+    assert captured.out == "Urs\n"
+
 
 def test_step3(capsys, monkeypatch):
-    inputs = iter(['Roberta', 'Charles', 'Daniela', 'Urs', 'Kyle', 2])
+    inputs = iter(['Roberta', 'Urs', 'Daniela', 'Charles', 'Kyle', 2])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
-    assert captured.out == "Charles\n"
+    assert captured.out == "Urs\n"
