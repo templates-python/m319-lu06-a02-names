f58a75b848a5152b0e6effb77409e89abdaa5c39
graphics80 <K.Ilkow@gmail.com>
2023-10-26 11:53:59 +0200

    Update givenname_test.py

--- givenname_test.py
+++ givenname_test.py
@@ -1,21 +1,17 @@
 from givenname import main
 
 
-def test_step1(capsys, monkeypatch):
-    inputs = iter(['Robert', 'Daniela', 'Urs', 'Charles', 'Kyle', 3])
-    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+def test_step1(capsys):
     main()
     captured = capsys.readouterr()
     assert captured.out == "Urs\n"
-
 
 def test_step2(capsys, monkeypatch):
-    inputs = iter(['Robert', 'Daniela', 'Urs', 'Charles', 'Kyle', 3])
+    inputs = iter(['Robert', 'Daniela', 'Urs', 'Charles', 'Kyle'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
     assert captured.out == "Urs\n"
-
 
 def test_step3(capsys, monkeypatch):
     inputs = iter(['Roberta', 'Urs', 'Daniela', 'Charles', 'Kyle', 2])
