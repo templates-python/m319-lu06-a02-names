d97da96142acf93bc2de421ad1a64d443ff38070
graphics80 <K.Ilkow@gmail.com>
2023-10-26 11:53:21 +0200

    Update givenname_test.py

--- givenname_test.py
+++ givenname_test.py
@@ -7,15 +7,15 @@ def test_step1(capsys):
     assert captured.out == "Urs\n"
 
 def test_step2(capsys, monkeypatch):
-    inputs = iter(['Robert', 'Daniela', 'Urs', 'Charles', 'Kyle'])
+    inputs = iter(['Robert', 'Daniela', 'Peter', 'Charles', 'Kyle'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
-    assert captured.out == "Urs\n"
+    assert captured.out == "Peter\n"
 
 def test_step3(capsys, monkeypatch):
-    inputs = iter(['Roberta', 'Urs', 'Daniela', 'Charles', 'Kyle', 2])
+    inputs = iter(['Roberta', 'Charles', 'Daniela', 'Urs', 'Kyle', 2])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     main()
     captured = capsys.readouterr()
-    assert captured.out == "Urs\n"
+    assert captured.out == "Charles\n"
